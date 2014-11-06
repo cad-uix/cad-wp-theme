db3b2f9e3d59926126e0bc3c7ba58e922e4ecf04
marcelbadua <bitterdash@gmail.com>
2014-11-06 09:56:47 +0800

    upgraded

--- inc/meta.php
+++ inc/meta.php
@@ -1,25 +1,31 @@
-<span>
-    <?php the_time('F jS, Y') ?>
-</span>
-
-  <span><?php the_author(); ?></span>
-  
 <?php
-$categories = get_the_category();
-$separator = ' ';
-$output = '';
-if($categories){
-	foreach($categories as $category) {
-		$output .= '<span class="label label-default">'.'<a href="'.get_category_link( $category->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts in %s" ), $category->name ) ) . '">'.$category->cat_name.'</a></span>'.$separator;
-	}
-echo trim($output, $separator);
-}
+/**
+ * Display Meta
+ *
+ * @package cad
+ * @author marcelbadua
+ */
 ?>
 
-<?php the_tags('<span class="label label-default">','</span> <span class="tag label label-default">', "</span>"); ?>
-
-
 <div class="meta">
+	<span>Posted by; <?php the_author(); ?> </span>
+	<span>on <?php the_time('F jS, Y') ?> </span>
+	
+	<span>
 
+	<?php
+		$categories = get_the_category();
+		$separator = ' ';
+		$output = '';
+		if($categories){
+			foreach($categories as $category) {
+				$output .= '<span class="label label-default">'.'<a href="'.get_category_link( $category->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts in %s" ), $category->name ) ) . '">'.$category->cat_name.'</a></span>'.$separator;
+			}
+		echo trim($output, $separator);
+		}
+		?>
 
+	<?php the_tags('<span class="label label-default">','</span> <span class="tag label label-default">', "</span>"); ?>
+			
+	</span>
 </div>
