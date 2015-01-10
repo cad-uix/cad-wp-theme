9cec997961bd3db34f3d96ce855e155c25d104e0
Marcel Badua <bitterdash@gmail.com>
2015-01-11 05:12:55 +0800

    Update header.php

--- header.php
+++ header.php
@@ -67,7 +67,7 @@
 
     <div id="slide-menu" class="load">
 
-        <?php cad_get_menu( 'off-canvas-navigation', 'nav'); ?>
+        <?php menu( 'off-canvas-navigation', 'nav'); ?>
         
     </div>
 
