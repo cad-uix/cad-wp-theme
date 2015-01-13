3c681f1535afb310c9c88cb379d1b13346d29657
Marcel Badua <bitterdash@gmail.com>
2015-01-13 17:00:09 +0800

    Update header.php

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
         elseif (is_archive()) {
             wp_title(''); echo ' Archive | '; }
         elseif (is_search()) {
-            echo 'Search for &quot;'.wp_specialchars($s).'&quot; | '; }
+            echo 'Search for &quot;'. $s .'&quot; | '; }
         elseif (!(is_404()) && (is_single()) || (is_page())) {
             wp_title(''); echo ' | '; }
         elseif (is_404()) {
